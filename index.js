165d2bf68fb79ed52179c388b3f7685e9dfb9bd1
lording <mathias.berthonneau@gmail.com>
2020-03-17 00:47:40 +0100

    test schedule

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ client.on('ready',async m => {
 
 
 
-cron.schedule('* * * * *', () => {
-	console.log('test every minute');
+cron.schedule('* 50,52 0 * tues', () => {
+	console.log('message  chaque mardi à 00:50 et 00:52');
 }, {
 	scheduled: true,
 	timezone: "Europe/Paris"
