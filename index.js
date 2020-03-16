7e849eb2eb55eac6821024fe02d536845938ec40
lording <mathias.berthonneau@gmail.com>
2020-03-16 22:50:29 +0100

    ajout commande ping, help et réponse async

--- index.js
+++ index.js
@@ -4,22 +4,68 @@ const client = new Discord.Client();
 
  
 
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+client.on('ready',async m => {
+	console.log(`logged in as ${client.user.tag}, in ${client.channels.size} channels of ${client.guilds.size} server.`);
+	client.user	.setActivity(`C'est pas les vacances!`)	
 });
 
  
 
-client.on('message', message => {
+client.on('message', async message => {
+	
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+		const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+		console.log(`Command ${command} by ${message.author.username}#${message.author.discriminator} in '${message.guild}' at ${message.createdAt}`);
+		if (args) console.log(`With argu ${args}`);
 
-    if (message.content === 'ping') {
+// help
+	if (command === "help") {
+		const help = require("./help.json");
+		var prefix=config.prefix+" ";
+		var helpHeader="```css\n";
+		var helpTitle;
+		var helpBody;
+		var helpFooter="\n```";
+		switch (`${args}`) {
+			case '2':
+				helpTitle=help.p2.Title+"\t[2/3]\n\n";
+				helpBody=help.p2.body;
+				helpFooter="\n\n\t"+prefix+" {help 3} pour les commandes Admins```";
+			 break;
 
-       message.reply('pong');
+			case '3':
+				helpTitle=help.p3.Title+"\t[3/3]\n\n";
+				helpBody=help.p3.body;
+			 break;
+			
+			case '1':
+			default:
+				helpTitle=help.p1.Title+"\t[1/3]\n\n";
+				helpBody=help.p1.body;
+				if (!`${args}`) 	helpFooter="\n\n\t"+prefix+" {help 2} pour les commandes Randoms```";
+			 break;
+		}
+		message.channel.send(helpHeader+helpTitle+helpBody+helpFooter);
+	}
 
-       }
+// addme
+	if (command === "addme") {
+		message.reply('https://discordapp.com/oauth2/authorize?client_id=689215093501591553&permissions=3072&scope=bot');
+	}
 
+
+// ping
+	if (command === "ping") {
+		var embed = new Discord.RichEmbed();
+		embed.setColor('#FFFFFF');
+		const m = await message.channel.send("Ping?");
+		embed.setAuthor(`${message.author.username} -> ping`, `${message.author.displayAvatarURL}`);
+		embed.addField(`Pong! (${m.createdTimestamp - message.createdTimestamp}ms).`,`Latence API: ${Math.round(client.ping)}ms.`);
+		message.delete();
+		m.delete();
+		message.channel.send(embed);
+	}
 });
 
  
