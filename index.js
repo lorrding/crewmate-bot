574401b5997875da771ff4dcb8804cdadc89b639
lording <mathias.berthonneau@gmail.com>
2020-03-17 12:22:13 +0100

    test scheduled message

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const dw = {role:"<@&604658418980093993>", channel:"<@#689193318743015429>"};
 const PremAnnee = {role:"<@&604658039189929994>", channel:"<@#689193168805036172>"};
 
 //15h30
-cron.schedule('20 12 * * tues', () => {
-	client.channels.get(`${eci.channel}`).send(`<@&651493555629457418>, petit test!`);
+cron.schedule('23 12 * * tues', () => {
+	client.channels.get(`689216081276960863`).send(`<@&651493555629457418>, petit test!`);
 }, {
 	scheduled: true,
 	timezone: "Europe/Paris"
