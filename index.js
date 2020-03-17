ad5fef7cd69895e28ec2b20bd6a6d45bfdc6236c
lording <mathias.berthonneau@gmail.com>
2020-03-17 02:59:08 +0100

    modification du statue + url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cron = require('node-cron');
 
 client.on('ready',async m => {
 	console.log(`logged in as ${client.user.tag}, in ${client.channels.size} channels of ${client.guilds.size} server.`);
-	client.user.setPresence({ activity: { name: 'C\'est pas les vacances!', type: '1'  }, status: 'idle' })
+	client.user.setPresence({ activity: { name: 'C\'est pas les vacances!', type: "STREAMING", url: "https://youtu.be/dQw4w9WgXcQ"}, status: 'idle' })
   		.then(console.log)
   		.catch(console.error);
 });
