c32ca012780f0a8cd40579cebfa3dd369850891b
mathias.b <mathias.berthonneau@gmail.com>
2020-10-19 22:08:35 +0200

    création liste de joueurs

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 		console.log(`id du message réagis: ${reaction.message.id} //// id du message de base ${gameMessage.id}`);
 	}
 
-	if (listJoueurs.size() <10) {
+	if (listJoueurs.size <10) {
 		console.log('taille de joueurs: ok, on insert');
 		listJoueurs.push(user.username);
 		message.channel.send(`liste des joueurs: ${listJoueurs}`);
