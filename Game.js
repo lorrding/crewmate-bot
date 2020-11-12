f24aefe750ff3a39a8e82b2ffffdda1a0737ed15
lording <mathias.berthonneau@gmail.com>
2020-11-12 01:41:01 +0100

    update to v12 + new api version

--- Game.js
+++ Game.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js')
-const { sendThenDelete, formatEmbedTime, getHexa } = require('./toolbox')
+const { sendThenDelete, formatEmbedTime, formatListPlayers, getHexa } = require('./toolbox')
 
 class Game {
-		
-	#guild
-	#role
-	#channel
-	#message = new Discord.Message()
-	#author = new Discord.User()
-	#listPlayers = []
-	#hours
-	#minutes
-	static #EMOJI = "764917952600342539"
+
+	#guild //guild the game is scheduled
+	#channel //channel the game is scheduled
+	#role //role to manage players
+	#message = new Discord.Message(undefined, undefined, undefined) // embed that represent the game
+	#author //= new Discord.User(new Discord.Client(), undefined) // author of the game
+	#listPlayers = [] // list of all players, excluding author
+	#hours // hours of the game
+	#minutes // minutes of the game
+	static #EMOJI = "764917952600342539" // static emoji for adding players
 
 	constructor(message, hours, minutes) {
 		this.#guild = message.guild
-		this.#role = message.guild.roles.find(r => r.name === "joueurDuSoir");
+		this.#role = message.guild.roles.cache.find(r => r.name === "joueurDuSoir");
 		this.#channel = message.channel
 		this.#author = message.author
 		this.#hours = hours
@@ -26,84 +26,129 @@ class Game {
 
 	addPlayer(reaction, member) {
 		//if wrong message, then don't
-		if (reaction.message.id != this.#message.id) return 0
+		if (reaction.message.id !== this.#message.id) return 0
 
 		//if wrong reaction, then don't
-		if (reaction.emoji.id != Game.#EMOJI) return 0
+		if (reaction.emoji.id !== Game.#EMOJI) return 0
 
 		// if author of game, then don't
 		console.log('author: '+this.getAuthor());
-		if(member.id == this.#author.id) {
+		if(member.id === this.#author.id) {
 			try {
-				reaction.remove(member)
+				reaction.users.remove(member).then(() => {})
 			} catch (error) {
-				return sendThenDelete(this.#channel, 5000, 'missing permission to remove reaction.')
+				return sendThenDelete(this.#channel, 'missing permission to remove reaction.')
 				.then(console.log(error))
 			}
-			sendThenDelete(this.#channel, 5000, "La personne qui propose de jouer est déjà dans la liste des joueurs, pas besoin de réagir au message!")
+			sendThenDelete(this.#channel, "La personne qui propose de jouer est déjà dans la liste des joueurs, pas besoin de réagir au message!")
 			return console.log('(from gObject) Author already un list, ignoring...')
 		}
 
 		//if already 10 players, then don't
 		if (this.#listPlayers.length >= 10) {
 			try {
-				reaction.remove(user)
+				reaction.users.remove(member).then(() => {})
 			} catch (error) {
-				return sendThenDelete(this.#channel, 5000, 'missing permission to remove reaction.')
+				return sendThenDelete(this.#channel, 'missing permission to remove reaction.')
 				.then(console.log(error))
 			}
-			sendThenDelete(this.#channel, 5000, "Nombre de joueurs max atteint!")
-			sendThenDelete(this.#channel, 5000, "<:AU_why:765273043962298410>")
+			sendThenDelete(this.#channel, "Nombre de joueurs max atteint!")
+			sendThenDelete(this.#channel, "<:AU_why:765273043962298410>")
 			console.log('Max players reached!');
 		}
 
 		//checking if user isn't already in list of players
-		if (listJoueurs.length > 0 && listJoueurs.find(user => user == user.username)) {
+		if (this.#listPlayers.length > 0 && this.#listPlayers.find(user => user === user.username)) {
 			try {
-				reaction.remove(user)
+				reaction.user.remove()
 			} catch (error) {
-				return sendThenDelete(this.#channel, 5000, 'missing permission to remove reaction.')
+				return sendThenDelete(this.#channel, 'missing permission to remove reaction.')
 				.then(console.log(error))
 			}
 			sendThenDelete(this.#channel ,5000, "Vous êtes déjà dans la liste des joueurs! (mais nous n'êtes même pas censé voir cette erreur)")
 			return console.log('user already un list, ignoring...')
 		}
 
-		//everything's good
+		//everything's ok
 		try {
 			// try adding user in list of players
 			console.log(`Adding user ${member.username}...`)
 			this.#listPlayers.push(`${member.username}`)
 			// editEmbed(reaction.message)
 		} catch (error) {
-			return sendThenDelete(this.#channel, 5000, `Erreur, impossible d'ajouter le jouer à la liste des participants.`)
+			return sendThenDelete(this.#channel, `Erreur, impossible d'ajouter le jouer à la liste des participants.`)
 			.then(console.log(error))
 		}
 
 		try {
 			//try adding role to player
 			let player = this.#guild.members.find(r => r.id === member.id)
-			player.addRole(this.#role)
+			player.roles.add(this.#role)
 		} catch (error) {
-			return sendThenDelete(this.#channel, 5000, 'missing permission to add role.')
+			return sendThenDelete(this.#channel, 'missing permission to add role.')
 			.then(console.log(error))
 		}
-		sendMessage();
+
+		//everything's good
+		this.editEmbed()
 	}
 
 	createEmbed() {
 		//creating embed
 		try {
-			let embed = new Discord.RichEmbed()
+			let embed = new Discord.MessageEmbed()
 			embed.setColor(getHexa())
-			embed.setAuthor(`${this.#author.username} propose de jouer`, `${this.#author.displayAvatarURL}`)
+			embed.setAuthor(`${this.#author.username} propose de jouer`, this.#author.displayAvatarURL)
 			embed.addField(`${formatEmbedTime(this.#hours, this.#minutes)} à:`,`${this.#hours}h${this.#minutes}`, true)
 			embed.addField(`Places restantes:`,`9`, true)
 			embed.setImage(`https://i.imgur.com/8sd2fgo.png`)
 			embed.setFooter(`Réagissez en dessous pour participer`)
 			return embed
 		} catch (error) {
-			throw sendThenDelete(this.#channel, 5000, "Erreur lors de la création de l'embed.")
+			throw sendThenDelete(this.#channel, "Erreur lors de la création de l'embed.")
+				.then(console.log(error))
+		}
+	}
+
+	editEmbed() {
+		let embed
+
+		//try copying embed
+		try {
+			embed = new Discord.MessageEmbed(this.#message.embeds[0])
+			embed.fields = []
+		} catch (error) {
+			sendThenDelete(this.#channel, "Error copying embed.")
+				.then(console.log(error))
+		}
+
+		//try updating fields
+		try {
+			embed.addField(`${formatEmbedTime(this.#hours, this.#minutes)} à:`,`${this.#hours}h${this.#minutes}`, true)
+
+			if (this.#listPlayers.length) {
+				embed.setDescription(`avec: ${formatListPlayers(this.#listPlayers)}`)
+				// let Nbplayers = 9 - this.#listPlayers.length;
+				embed.addField(`Places restantes:`,`${9 - this.#listPlayers.length}`, true)
+			} else {
+				embed.setDescription(``)
+				embed.addField(`Places restantes:`,`9`, true)
+			}
+		} catch (error) {
+			sendThenDelete(this.#channel, "Error updating fields.")
+				.then(console.log(error))
+		}
+
+		//try sending embed
+		try {
+			this.#channel.send(embed)
+				//adding reaction
+				.then(embedMessage => {
+					embedMessage.react(Game.#EMOJI).then(() => {})
+					this.#message = embedMessage
+				})
+		} catch (error) {
+			sendThenDelete(this.#channel, "missing permissions to send embed or react.")
 				.then(console.log(error))
 		}
 	}
@@ -114,20 +159,20 @@ class Game {
 			this.#channel.send(embed)
 			//adding reaction
 				.then(embedMessage => {
-					embedMessage.react(Game.#EMOJI)
+					embedMessage.react(Game.#EMOJI).then(() => {})
 					this.#message = embedMessage
 				})
 		} catch (error) {
-			sendThenDelete(this.#channel, 5000, "missing permissions to react or send embed.")
+			sendThenDelete(this.#channel, "missing permissions to react or send embed.")
 				.then(console.log(error))
 		}
 
 		try {
 			//try adding role to player
-			let player = this.#guild.members.find(r => r.id === this.#author.id)
-			player.addRole(this.#role)
+			let player = this.#guild.members.cache.find(r => r.id === this.#author.id)
+			player.roles.add(this.#role)
 		} catch (error) {
-			return sendThenDelete(this.#channel, 5000, 'missing permission to add role.')
+			sendThenDelete(this.#channel, 'missing permission to add role.')
 			.then(console.log(error))
 		}
 	}
@@ -140,9 +185,9 @@ class Game {
 		return this.#role
 	}
 
-	getChannel() {
-		return this.#channel
-	}
+	// getChannel() {
+	// 	return this.#channel
+	// }
 
 	getMessage() {
 		return this.#message
@@ -152,17 +197,17 @@ class Game {
 		return this.#author
 	}
 
-	getListPlayers() {
-		return this.#listPlayers
-	}
-
-	getHours() {
-		return this.#hours
-	}
-
-	getMinutes() {
-		return this.#minutes
-	}
+	// getListPlayers() {
+	// 	return this.#listPlayers
+	// }
+	//
+	// getHours() {
+	// 	return this.#hours
+	// }
+	//
+	// getMinutes() {
+	// 	return this.#minutes
+	// }
 
 	static getEmoji() {
 		return Game.#EMOJI
--- toolbox.js
+++ toolbox.js
@@ -1,21 +1,21 @@
 module.exports = {
 
-	//send a message, then delete it after x milisecondes
-	sendThenDelete : function (channel, ms, message) {
+	//send a message, then delete it after x millisecondes
+	sendThenDelete : function (channel, message, ms = 5000) {
 		try {
 			return channel.send(message)
 			.then(msg=> {
-				msg.delete(ms);
-			});
+				msg.delete({timeout: ms})
+			})
 		} catch (error) {
-			console.log(`Error deleting message...\n${error}`);	
+			console.log(`Error deleting message...\n${error}`);
 		}
 	},
 
 	// format text for game embed message
 	formatEmbedTime : function(hours, minutes) {
-		let str = "";
-		let date = new Date();
+		let str = ""
+		let date = new Date()
 		
 		//today or tomorrow ?
 		if (date.getUTCHours()+2 <= hours) { // utc+2 = summer
@@ -32,11 +32,11 @@ module.exports = {
 		if (18 <= hours < 3) {
 			str += "soir"
 		} else if (hours >= 14) {
-			if (str == "Ce ") {
-				str = str.slice(0, -1); 
-				str += "t aprèm"
+			if (str === "Ce ") {
+				str = str.slice(0, -1)
+				str += "t après-midi"
 			} else  {
-				str += "aprèm"
+				str += "après-midi"
 			}
 		} else if (str >= 12) {
 			str += "midi"
@@ -46,13 +46,33 @@ module.exports = {
 		return str
 	},
 
+	// format list of players
+	formatListPlayers : function(listPlayers) {
+		let listToString = ""
+		for (let i = 0; i < listPlayers.length; i++) {
+			if (listPlayers.length > 1) {
+				if (i >= listPlayers.length-1) {
+					//on remove ", "
+					listToString = listToString.slice(0, -2)
+					// et on met le dernier élément à la place
+					listToString+=` et ${listPlayers[i]}.`
+				} else {
+					listToString+=`${listPlayers[i]}, `
+				}
+			} else {
+				listToString+=`${listPlayers[i]}.`
+			}
+		}
+		return listToString
+	},
+
 	// random hexadecimal number for embeds color
 	getHexa : function () {
 		return '#'+Math.floor(Math.random()*16777215).toString(16);
 	},
 
 	// random number between to included integers
-	getRandom : function (min, max) {
-		return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) +1)) + Math.ceil(min);
-	}
+	// getRandom : function (min, max) {
+	// 	return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) +1)) + Math.ceil(min);
+	// }
 }
